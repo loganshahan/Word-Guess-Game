9ff83ff6aacadae023563dae926ad20d2a102427
Logan Shahan <loganshahan88@gmail.com>
2018-12-19 12:21:17 -0500

    added functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,79 @@
-var $ = function (id) {
-    return document.getElementById(id);
-}
-    
+var wordBank = ["queen", "aerosmith", "madona", "prince", "cher"];
+var randomWord = "";
+var lettersInRandom = [];
+var blanks = 0;
+var blanksAndLetters = [];
+var wrong = [];
+var letterGuessed = "";
 
-
-var game = ["queen", "aerosmith", "madona", "prince", "cher"];
-var choice = Math.floor(Math.random()*wordBank.length);
-var answer = game[choice];
-var myLength = answer.length;
-var display = [myLength];
-var win = myLength;
-var letters = answer.split('');
-var maxGuesses = 10;
+var win = 0;
 var losses = 0;
-var output = "";
-var userLetter = "";
+var guessesLeft = 10;
 
+function setUp(){
+  guessesLeft = 10;
+  randomWord =      wordsBank[Math.floor(Math.random() *  wordsBank.length)];
+   lettersInRandom = randomWord.split("");
+  blanks = lettersInRandom.length;
+  console.log(randomWord);
+  blanksAndLetters = [];
+  wrong = [];
+  for(var i = 0; i < blanks; i++){
+    blanksAndLetters.push("_");
+  }
+  console.log(blanksAndLetters);
+  document.getElementById("guesses-remaining").innerHTML = guessesLeft;
+  document.getElmentById("current-word").innerHTML = blanksAndLetters.join(" ");
+  document.getElementById("guessed-letters").innerHTML = wrong.join(" ");
 
-var setup = function()
-{
-    for(var i=0; i<answer.length; i++){
-        display[i] = "_ ";
-        output = output + display[i];
+};
+function check(letter){
+    var letterRight = false;
+    for (var i = 0; i < blanks; i++){
+        if (randomWord[i] === letter){
+           letterRight = true; 
+        }
+
     }
-    document.getElementById(game).innerHTML = output;
-    output = "";
-}
-var submit = function()
-{
-    
-}
+    if (letterRight){
+        for(var r = 0; r < blanks; r++){
+            if(randomWord[r] === letter){
+                blanksAndLetters[r] = letter;
+            }
+        }
+        console.log(blanksAndLetters);
+    }
+    else{
+        wrong.push(letter);
+        guessesLeft --;
+    }
+   
+};
+function done(){
+    console.log("wins: " + win + " | losses: " + losses + " | guesses: " + guessesLeft);
+
+    document.getElementById("guesses-remaining").innerHTML = guessesLeft;
+    document.getElementById("current-word").innerHTML = blanksAndLetters.join(" ");
+    document.getElementById("guessed-letters").innerHTML = wrong.join(" ");
+
+    if(lettersInRandom.toString() === blanksAndLetters.toString()){
+        win++;
+        document.getElementById("wins").innerHTML = win;
+        setUp();
+    }
+    else if(guessesLeft ===0){
+        losses++;
+        setUp();
+
+    }
+};
+
+setUp();
+document.onkeyup = function(event){
+    letterGuessed = String.fromCharCode(event.which).toLocaleLowerCase();
+    check(letterGuessed);
+    done();
+
+};
 
 
